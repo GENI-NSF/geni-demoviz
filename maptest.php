ed1e5f9b51de73800382915eee7b6b17e4c28061
Tom Mitchell <tmitchell@bbn.com>
2015-03-06 16:23:31 -0500

    Do not sign the user in to google.

--- maptest.php
+++ maptest.php
@@ -50,7 +50,7 @@ if (array_key_exists('zoom', $_GET)) {
 <head>
 
 <script type="text/javascript" src="https://www.google.com/jsapi"></script>
-<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=true"></script>
+<script src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
 
