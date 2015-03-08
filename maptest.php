6fb5c233ae006b99b913b3f98d52899a82408b45
Tom Mitchell <tmitchell@bbn.com>
2015-03-08 08:57:19 -0400

    Tweak HTML for standards and consistency.

--- maptest.php
+++ maptest.php
@@ -46,10 +46,11 @@ if (array_key_exists('zoom', $_GET)) {
 ?>
 <!DOCTYPE HTML>
 <html lang="en">
-<meta charset="utf-8">
 <head>
+<meta charset="utf-8">
+<title>GEC 22 Map Test</title>
 <link rel="stylesheet" href="map.css">
-<script type="text/javascript" src="https://www.google.com/jsapi"></script>
+<script src="https://www.google.com/jsapi"></script>
 <script src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
