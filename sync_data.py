39e96a50bdd18f6329dcd32442430d682772d8a8
Marshall Brinn <mbrinn@bbn.com>
2015-03-11 07:29:39 -0400

    Fixes to sync_data

--- sync_data.py
+++ sync_data.py
@@ -1,4 +1,4 @@
-!/usr/bin/env python                                                                            
+#!/usr/bin/env python                                                                            
 #----------------------------------------------------------------------                          
 # Copyright (c) 2013-2015 Raytheon BBN Technologies                                              
 #                                                                                                
@@ -31,7 +31,7 @@ import os
 import sys
 
 # Names of hosts for which there will be files                                                   
-hosts = ['canarie', 'uchicago', 'ebi', 'nih'];
+hosts = ['canarie', 'chicago', 'ebi', 'nih', 'starlight'];
 
 # Class to synchronize data                                                                      
 class DataSynchronizer:
@@ -56,6 +56,7 @@ class DataSynchronizer:
         parser.add_option("--dbpass", help="Database password", default="0mlisg00d4u")
         parser.add_option("--dbport", help="Database port", default="5432")
         parser.add_option("--dbtable", help="Name of database table")
+        parser.add_option("--dbtable_link", help="Name of database link table")
         parser.add_option('--dbfield', help="Name of field of value")
 
     # Check that all required parser arguments are provided                                      
@@ -66,14 +67,16 @@ class DataSynchronizer:
         if opts.dbtable== None: required_missing_fields.append('dbtable')
         if opts.dbfield== None: required_missing_fields.append('dbfield')
 
-        if len(required_missing_fields) > 0:
+	if len(required_missing_fields) > 0:
             raise Exception("Missing required arguments: " +
                             " ".join(required_missing_fields))
 
     # Name of filename for given host (Internaltion SDX demo specific)                           
-    def filename_for_host(self, host):
-        return "/tmp/%s2slSDX" % host
-
+    def filename_for_host(self, host, towards_sl):
+	if towards_sl:
+            return "/tmp/links/%s2slSDX" % host
+	else:
+            return "/tmp/links/slSDX2%s" % host
 
     # Parse command line options                                                                 
     def parse_options(self):
@@ -84,46 +87,67 @@ class DataSynchronizer:
        self.check_options(opts)
        return opts
 
-    # Create an SQL file with appropriate transaction boundaries                                 
-    # To clean out old tables and enter new entries                                              
-    # Then execute the SQL file                                                                  
-    def run(self):
-        # Open file                                                                              
-        sql_file = open(self._opts.outfile, 'wb')
-        # Add 'begin transaction;"                                                               
-        sql_file.write('begin transaction;\n')
-        for host in hosts:
-            if self._latest_time_per_host[host] == None:
-                sql_file.write("delete from %s where sender = '%s';\n" % \
-                                   (self._opts.dbtable, host))
-            host_filename = self.filename_for_host(host)
-            data_exists = False
-            host_data = None
-            try:
-                host_data = open(host_filename, 'r').read()
-                data_exists = True
-            except:
-                pass
-            if not data_exists: continue
+    def link_statement(self, sql_file, host, status):
+        update_statement = "update %s set status = '%s' where link_id = '%s-link';\n" % \
+            (self._opts.dbtable_link, status, host)
+        sql_file.write(update_statement);
+
+    # Parse given file (indicated by name and towards_sl)                                        
+    # If exists, parse and generate insert statements                                            
+    # Otherwise, return false                                                                    
+    def process_file(self, sql_file, host, towards_sl):
+        host_filename = self.filename_for_host(host, towards_sl)
+        data_exists = False
+        host_data = None
+        try:
+            host_data = open(host_filename, 'r').read()
+            data_exists = True
+        except:
+            pass
+        if data_exists:
             print "Processing file %s" % host_filename
             lines = host_data.split('\n');
             for line in lines:
                 if line == '': continue
                 parts = line.split('\t');
-                ts = int(parts[0])
+		ts = int(parts[0])
                 value = int(parts[1])
                 insert_stmt = "insert into %s (ts, %s, sender) values (%d, %d, '%s');\n" % \
                     (self._opts.dbtable, self._opts.dbfield, ts, value, host)
                 if ts > self._latest_time_per_host[host]:
                     sql_file.write(insert_stmt)
                     self._latest_time_per_host[host] = ts
+        return data_exists
+
+    # Create an SQL file with appropriate transaction boundaries                                 
+    # To clean out old tables and enter new entries                                              
+    # Then execute the SQL file                                                                  
+    def run(self):
+        # Open file                                                                              
+	sql_file = open(self._opts.outfile, 'wb')
+        # Add 'begin transaction;"                                                               
+	sql_file.write('begin transaction;\n')
+        for host in hosts:
+            if self._latest_time_per_host[host] == None:
+                sql_file.write("delete from %s where sender = '%s';\n" % \
+                                   (self._opts.dbtable, host))
+            towards_exists = self.process_file(sql_file, host, True)
+            from_exists = self.process_file(sql_file, host, False)
+            if towards_exists or from_exists:
+		# Enable link                                                                    
+		self.link_statement(sql_file, host, 'up');
+            else:
+                # Disable link                                                                   
+                self.link_statement(sql_file, host, 'down');
+
+
         # Add 'commit';                                                                          
         sql_file.write('commit;\n')
         # Close file                                                                             
         sql_file.close()
 
         # Invoke SQL file                                                                        
-        sql_cmd = "PGPASSWORD=%s psql -U %s -h %s %s -f %s" % \
+	sql_cmd = "PGPASSWORD=%s psql -U %s -h %s %s -f %s" % \
             (self._opts.dbpass, self._opts.dbuser, self._opts.dbhost,
              self._opts.dbname, self._opts.outfile)
         print "INVOKING CMD = %s" % sql_cmd
@@ -135,3 +159,10 @@ def main():
     while True:
         sd.run()
         time.sleep(5)
+
+
+
+
+if __name__ == "__main__":
+    sys.exit(main())
+
