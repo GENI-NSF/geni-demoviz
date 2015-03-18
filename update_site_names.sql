751422ca584d9752f0a9bd108d8f308e2e3d6d20
Marshall Brinn <mbrinn@bbn.com>
2015-03-18 09:48:20 -0400

    Update gec22.html urls, update site names to remove EG/IG, add new Iminds URL to lively

---
+++ update_site_names.sql
@@ -0,0 +1,103 @@
+id  |                                    am_urn                                    |                 am_name                  | longitude | latitude |                                        icon                                        
+-----+------------------------------------------------------------------------------+------------------------------------------+-----------+----------+------------------------------------------------------------------------------------
+ 326 | urn:publicid:IDN+wix.internet2.edu+authority+am                              | Washington Internet Exchange             |   -77.175 |   38.928 | 
+ 327 | urn:publicid:IDN+dragon.maxgigapop.net+authority+am                          | MAX SDX                                  |    -76.92 |    38.97 | 
+ 328 | urn:publicid:IDN+instageni.cenic.net+authority+cm                            | CENIC INSTAGENI                          |   -118.26 |   34.049 | http://merit.edu/cenic/images/CENIC_logo.png
+ 329 |                                                                              | AMAZON AWS                               |    -83.72 |    36.61 | http://www.freelargeimages.com/wp-content/uploads/2014/11/Amazon_logo-8.png
+ 288 | urn:publicid:IDN+apt.emulab.net+authority+cm                                 | Apt                                      |   -111.89 |    40.75 | 
+ 238 | urn:publicid:IDN+exogeni.net:slvmsite+authority+am                           | StarLight ExoGENI                        |    -87.68 |    42.05 | http://www.startap.net/starlight/images/emblemSubLevLogo.gif
+ 245 | urn:publicid:IDN+instageni.maxgigapop.net+authority+cm                       | MAX InstaGENI                            |    -76.92 |    38.97 | http://www.maxgigapop.net/wp-content/themes/maxtheme/images/logo.png
+ 261 | urn:publicid:IDN+exogeni.net:bbnvmsite+authority+am                          | GPO ExoGENI                              |    -71.15 |    42.39 | http://www.geni.net/favicon.ico
+ 300 | urn:publicid:IDN+instageni.nysernet.org+authority+cm                         | NYSERNet InstaGENI                       |    -76.15 |    43.05 | /common/nysernet-favicon.ico
+ 262 | urn:publicid:IDN+exogeni.net:nictavmsite+authority+am                        | NICTA ExoGENI                            |     151.2 |    -33.9 | /common/nicta-favicon.ico
+ 260 | urn:publicid:IDN+wall2.ilabt.iminds.be+authority+cm                          | iMinds Virtual Wall 2                    |      3.73 |    51.04 | /common/iminds-favicon.ico
+ 263 | urn:publicid:IDN+instageni.rnet.missouri.edu+authority+cm                    | Missouri InstaGENI                       |    -92.33 |    38.95 | /common/mizz-favicon.ico
+ 257 | urn:publicid:IDN+exogeni.net:osfvmsite+authority+am                          | OSF ExoGENI                              |   -122.25 |    37.88 | /common/doe-favicon.ico
+ 259 | urn:publicid:IDN+exogeni.net:uflvmsite+authority+am                          | UFL ExoGENI                              |    -82.35 |    29.64 | /common/ufl-favicon.ico
+ 240 | urn:publicid:IDN+exogeni.net:uhvmsite+authority+am                           | UH ExoGENI                               |    -95.34 |    29.72 | 
+ 241 | urn:publicid:IDN+emulab.net+authority+cm                                     | Utah ProtoGENI                           |   -111.85 |    40.77 | 
+ 246 | urn:publicid:IDN+geni.uchicago.edu+authority+cm                              | University of Chicago InstaGENI          |     -87.6 |    41.79 | 
+ 247 | urn:publicid:IDN+genirack.nyu.edu+authority+cm                               | NYU InstaGENI                            |       -74 |    40.73 | 
+ 250 | urn:publicid:IDN+geni.kettering.edu+authority+cm                             | Kettering InstaGENI                      |    -83.71 |    43.01 | 
+ 254 | urn:publicid:IDN+lan.sdn.uky.edu+authority+cm                                | Kentucky InstaGENI                       |     -84.5 |    38.04 | 
+ 255 | urn:publicid:IDN+instageni.rnoc.gatech.edu+authority+cm                      | Georgia Tech InstaGENI                   |     -84.4 |    33.78 | 
+ 256 | urn:publicid:IDN+pks2.sdn.uky.edu+authority+cm                               | Kentucky PKS2 InstaGENI                  |     -84.5 |    38.03 | 
+ 258 | urn:publicid:IDN+instageni.ku.gpeni.net+authority+cm                         | Kansas InstaGENI                         |    -95.25 |    38.96 | 
+ 264 | urn:publicid:IDN+instageni.metrodatacenter.com+authority+cm                  | Ohio Metro Data Center InstaGENI         |    -83.13 |    40.09 | 
+ 266 | urn:publicid:IDN+instageni.iu.edu+authority+cm                               | MOXI InstaGENI                           |     -86.5 |    39.17 | 
+ 268 | urn:publicid:IDN+instageni.clemson.edu+authority+cm                          | Clemson InstaGENI                        |    -82.84 |    34.68 | 
+ 271 | urn:publicid:IDN+uky.emulab.net+authority+cm                                 | Kentucky ProtoGENI                       |     -84.5 |    38.04 | 
+ 272 | urn:publicid:IDN+instageni.stanford.edu+authority+cm                         | Stanford InstaGENI                       |   -122.17 |    37.43 | 
+ 280 | urn:publicid:IDN+instageni.northwestern.edu+authority+cm                     | Northwestern InstaGENI                   |    -87.62 |     41.9 | 
+ 281 | urn:publicid:IDN+utah.geniracks.net+authority+cm                             | Utah InstaGENI                           |   -111.85 |    40.77 | 
+ 286 | urn:publicid:IDN+instageni.rutgers.edu+authority+cm                          | Rutgers InstaGENI                        |    -74.45 |    40.47 | 
+ 317 |                                                                              | EMBL-European Biometrics Institude       |     0.186 |    52.08 | /common/dataset_ebi.png
+ 239 | urn:publicid:IDN+utah.cloudlab.us+authority+cm                               | Utah CloudLab                            |   -111.89 |    40.75 | /common/Cloudlab-small-logo.jpeg
+ 248 | urn:publicid:IDN+exogeni.net:wvnvmsite+authority+am                          | WVNET ExoGENI                            |    -78.68 |    35.77 | /common/wvnet-logo.jpg
+ 291 | urn:publicid:IDN+instageni.nps.edu+authority+cm                              | NPS InstaGENI                            |   -121.87 |     36.6 | 
+ 293 | urn:publicid:IDN+instageni.illinois.edu+authority+cm                         | Illinois InstaGENI                       |     -88.2 |    40.11 | 
+ 294 | urn:publicid:IDN+exogeni.net:wsuvmsite+authority+am                          | Wayne State ExoGENI                      |    -83.07 |    42.36 | 
+ 303 | urn:publicid:IDN+instageni.wisc.edu+authority+cm                             | Wisconsin InstaGENI                      |    -89.41 |    43.08 | 
+ 305 | urn:publicid:IDN+exogeni.net:rcivmsite+authority+am                          | RENCI ExoGENI                            |    -79.02 |    35.94 | 
+ 315 | urn:publicid:IDN+instageni.sox.net+authority+cm                              | SoX InstaGENI                            |    -84.39 |    33.75 | 
+ 307 | urn:publicid:IDN+geni.it.cornell.edu+authority+cm                            | Cornell InstaGENI                        |    -76.48 |    42.45 | http://www.cornell.edu/assets/core/images/logo-red.png
+ 292 | urn:publicid:IDN+exogeni.net:ucdvmsite+authority+am                          | UC Davis ExoGENI                         |   -121.75 |    38.54 | http://marketingtoolbox.ucdavis.edu/local_resources/images/logos/Informal-Seal.png
+ 274 | urn:publicid:IDN+instageni.idre.ucla.edu+authority+cm                        | UCLA InstaGENI                           |   -118.44 |    34.07 | http://brand.ucla.edu/wp-content/uploads/2013/08/ucla-wordmark-main-1-600x286.jpg
+ 289 | urn:publicid:IDN+instageni.gpolab.bbn.com+authority+cm                       | GPO InstaGENI                            |    -71.15 |    42.39 | http://www.geni.net/favicon.ico
+ 306 | urn:publicid:IDN+utahddc.geniracks.net+authority+cm                          | UtahDDC InstaGENI                        |   -111.89 |    40.75 | /common/utah-favicon.ico
+ 323 |                                                                              | University of Southern California        |   -118.28 |    34.02 | /common/usc_microscope-v2.png
+ 324 |                                                                              | Crystal City                             |   -77.052 |    38.85 | /common/usc_DC.png
+ 325 |                                                                              | STEM School Chatanooga                   |    -85.26 |    35.04 | /common/usc_UTC.png
+ 318 |                                                                              | CANARIE                                  |    -75.69 |    45.42 | /common/dataset_canarie.png
+ 319 |                                                                              | NIH                                      |     -77.1 |       39 | /common/dataset_nih.png
+ 322 |                                                                              | StarLight SDX                            |    -87.61 |   41.895 | /common/starlight_sdx.png
+ 321 |                                                                              | StarLight                                |    -87.61 |   41.895 | /common/dataset_starlight.png
+ 299 | urn:publicid:IDN+exogeni.net:pscvmsite+authority+am                          | exogeni.net:pscvmsite                    |    -79.97 |    40.44 | /common/PGHCompLogo.png
+ 320 |                                                                              | U. Chicago                               |    -87.72 |    41.71 | /common/compute_chicago.png
+ 330 |                                                                              | Potsdam, Germany                         |    13.066 |     52.4 | /common/HPI-germany.jpeg
+ 332 |                                                                              | University of Victoria                   |   -123.31 |   48.465 | /common/victoria-logo.jpeg
+ 335 |                                                                              | Argonne NL                               |   -87.982 |     42.5 | /common/argonne-logo.jpeg
+ 296 | urn:publicid:IDN+exogeni.net:fiuvmsite+authority+am                          | FIU ExoGENI                              |    -80.37 |    25.76 | /common/fiu-logo.jpeg
+ 333 |                                                                              | Bonfire                                  |     -3.19 |    55.95 | /common/bonfire-square.jpeg
+ 334 |                                                                              | NERSC-LBL                                |   -122.24 |     38.5 | /common/OSF-NERSC-logo.png
+ 331 |                                                                              | Tokyo, Japan                             |    139.68 |    35.68 | /common/uoTokyo-logo.jpeg
+ 276 | urn:publicid:IDN+exogeni.net:tamuvmsite+authority+am                         | Texas A&M ExoGENI                        |    -96.34 |    30.62 | /common/texasam-logo.png
+
+update allsites set am_name ='PSC' where id = 299;
+update allsites set am_name = 'UtahDDC' where id = 306;
+update allsites set am_name = 'CENIC' where id = 328;
+update allsites set am_name = 'StarLight' where id = 238;
+update allsites set am_name = 'MAX' where id = 245;
+update allsites set am_name = 'NYSERNet' where id = 300;
+update allsites set am_name = 'NICTA' where id = 262;
+update allsites set am_name = 'Missouri' where id = 263;
+update allsites set am_name = 'OSF' where id = 257;
+update allsites set am_name = 'Florida' where id = 259;
+update allsites set am_name = 'U. of Houston' where id = 240;
+update allsites set am_name = 'U. of Chicago' where id = 246;
+update allsites set am_name = 'NYU' where id = 247;
+update allsites set am_name = 'Kettering' where id = 250;
+update allsites set am_name = 'Kentucky' where id = 254;
+update allsites set am_name = 'GA Tech' where id = 255;
+update allsites set am_name = 'Kentucky PKS2' where id = 256;
+update allsites set am_name = 'Kansas' where id = 258;
+update allsites set am_name = 'Ohio Metro Data Center' where id = 264;
+update allsites set am_name = 'MOXI' where id = 266;
+update allsites set am_name = 'Clemson' where id = 268;
+update allsites set am_name = 'Stanford' where id = 272;
+update allsites set am_name = 'Northwestern' where id = 280;
+update allsites set am_name = 'Utah' where id = 281;
+update allsites set am_name = 'Rutgers' where id = 286;
+update allsites set am_name = 'WVNET' where id = 248;
+update allsites set am_name = 'NPS' where id = 291;
+update allsites set am_name = 'Illinois' where id = 293;
+update allsites set am_name = 'Wayne State' where id = 294;
+update allsites set am_name = 'Wisconsin' where id = 303;
+update allsites set am_name = 'RENCI' where id = 305;
+update allsites set am_name = 'SoX' where id = 315;
+update allsites set am_name = 'Cornell' where id = 307;
+update allsites set am_name = 'UC Davis' where id = 292;
+update allsites set am_name = 'UCLA' where id = 274;
+
+
+
